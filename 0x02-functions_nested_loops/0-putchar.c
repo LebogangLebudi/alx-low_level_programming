8ebb82156cd065d826d36c23ff918441a6fdb97d
Lebogang Lebudi <lebogang222@gmail.com>
2023-02-21 16:08:14 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,18 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always returns 0
- *
+ * main - print _putchar
+ * Return: Aways 0
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
